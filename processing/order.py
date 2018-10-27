b65d01d0ff100a98b81ac553491869bd5bdcf35e
jenxiao9 <just.jelly.here@gmail.com>
2018-10-26 22:56:05 -0400

    processing code

--- processing/order.py
+++ processing/order.py
@@ -1,7 +1,29 @@
-
+import datetime
 
 class Order:
-    def __init__(self):
-        pass
+    def __init__(self, t_date, stock_symbol, expiration, strike, call_put, 
+            symbol, price_bid, price_ask, price_last, date_bid, date_ask, date_last, 
+            size_bid, size_ask, volume, iv,delta, gamma, theta, vega, rho, pre_iv):
 
-    
+        self.t_date = t_date
+        self.stock_symbol = stock_symbol
+        self.expiration = expiration
+        self.strike = strike
+        self.call_put = call_put
+        self.price_bid = price_bid
+        self.price_ask = price_ask
+        self.price_last = price_last
+        self.date_bid = date_bid
+        self.date_ask = date_ask
+        self.date_last = date_last
+        self.size_bid = size_bid
+        self.size_ask = size_ask
+        self.volume = volume
+        self.iv = iv
+        self.delta = delta
+        self.gamma = gamma
+        self.theta = theta
+        self.vega = vega
+        self.rho = rho
+        self.pre_iv = pre_iv
+
--- processing/processing.py
+++ processing/processing.py
@@ -1,4 +1,41 @@
 import sys
-
+import csv
 import order
 
+def process_orders(file):
+    rows = 0
+    with open(file, "r") as f:
+        order_reader = csv.reader(f, delimiter=",")
+        for row in order_reader:
+            rows += 1
+            print(row)
+            if len(row) != 28:
+                raise Exception("Row in CSV does not have expected number of items")
+            [t_date, stock_id, stock_symbol, expiry, strike, call_put, style, symbol, 
+            price_bid, price_ask, price_last, date_bid, date_ask, date_last, size_bid, 
+            size_ask, exchange_bid, exchange_ask, volume, iv, delta, 
+            gamma, theta, vega, rho, pre_iv, implied_yield, calc_date] = row
+            
+            # we need: t_date, stock_symbol, expiration date, strike, call_put, 
+            # symbol (option symbol), price_bid, price_ask, date_bid, date_ask,
+            # size_bid, size_ask, volume, iv, price_opt, delta, gamma, theta, 
+            # vega, rho, pre_iv
+            current_order = order.Order(t_date, stock_symbol, expiry, strike, call_put, 
+            symbol, price_bid, price_ask, price_last, date_bid, date_ask, date_last, 
+            size_bid, size_ask, volume, iv,delta, gamma, theta, vega, rho, pre_iv)
+
+            if rows == 10:
+                break
+def send_order(order):
+    pass 
+
+
+
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print(len(sys.argv))
+        raise Exception("Please give an input file to process")
+    else:
+        process_orders(sys.argv[1])
